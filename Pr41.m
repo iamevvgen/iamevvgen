435c26a1402986791fb3b0ee4e6682d3babb5b2b
GavVlad <94898556+GavVlad@users.noreply.github.com>
2021-12-19 16:52:53 +0400

    Add files via upload

---
+++ Pr41.m
@@ -0,0 +1,12 @@
+
+a1 = xlsread('SokSerg.xlsx',1,'E8:E4386');
+a2 = xlsread('SokSerg.xlsx',1,'K8:K4386');
+a3 = xlsread('SokSerg.xlsx',1,'O8:O4386');
+a4 = xlsread('SokSerg.xlsx',1,'Q8:Q4386');
+a5 = xlsread('SokSerg.xlsx',1,'S8:S4386');
+a6 = xlsread('SokSerg.xlsx',1,'T8:T4386');
+data = [a1 a2 a3 a4 a5 a6]
+
+
+
+
